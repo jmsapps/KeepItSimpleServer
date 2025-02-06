93be3a8af3789ffa0f6a2ccb321c32b9b0e9b09b
Jeff Santos <jmsapps@outlook.com>
2025-02-06 18:06:56 -0500

    revert example file

--- example.py
+++ example.py
@@ -31,68 +31,10 @@ class HomeHandler(BaseHandler):
 		self.respond({"message": f"Successful POST to HomeHandler! -> {self.params}"}, 201)
 
 
-class TreatmentHandler(BaseHandler):
-	def get(self, userId):
-
-		# try-catch
-		# user = getUser(db, userId)
-
-		# getTreatments = getTreatments(db, user["id"])
-
-		# getImageUrl
-
-		treatments = [
-				{
-						"userId": 1,
-						"label": "Weight loss Treatment",
-						"ailment": "weight-loss",
-						"action-items": "checkup",
-						"due-date": "some date",
-						"isRequiredReq": True
-				},
-		]
-
-		self.respond({"message": {"treatments": treatments}})
-
-
-class ActionItemsHandler(BaseHandler):
-	def get(self, userId):
-
-		actionItems = [
-			{
-				"label": "Checkup",
-				"value": "checkup"
-			}
-		]
-
-		print(userId, type(userId))
-
-		user = None
-
-		users = [{
-			"id": 1,
-			"action-items": actionItems
-		}]
-
-		for u in users:
-			if u["id"] == userId:
-				user = u
-				break
-
-		if not user:
-			self.send_error(404, f"user with id '{userId}' not found")
-
-			return
-
-		self.respond({"message": {"action-items": user["action-items"]}})
-
-
 def run_server(serverType="sync", port=8080):
 	RequestHandler.routes = {
 		r"/": HomeHandler,
 		r"/([\d]+)": HomeHandler,
-		r"/treatments": TreatmentHandler,
-		r"/actionitems/([\d]+)": ActionItemsHandler,
 	}
 
 	RequestHandler.dbConnection = {
